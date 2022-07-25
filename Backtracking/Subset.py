75b1c9685f88bfc76454e75bf7dbfd5062842925
Rahul Ranjan <rrahulranjan26@gmail.com>
2022-07-26 03:00:04 +0530

    updated subset

--- Backtracking/Subset.py
+++ Backtracking/Subset.py
@@ -5,18 +5,18 @@ class Solution:
     def subsets(self, A):
         A.sort()
 
-    ans = []
-    idx = 0
+        ans = []
+        idx = 0
 
-    self.solver(A, ans, idx, [])
-    return sorted(ans)
+        self.solver(A, ans, idx, [])
+        return sorted(ans)
 
 
-def solver(self, A, ans, idx, ssf):
-    if idx == len(A):
-        ans.append(ssf)
-        return
-    self.solver(A, ans, idx + 1, ssf + [A[idx]])
-    self.solver(A, ans, idx + 1, ssf)
+    def solver(self, A, ans, idx, ssf):
+        if idx == len(A):
+            ans.append(ssf)
+            return
+        self.solver(A, ans, idx + 1, ssf + [A[idx]])
+        self.solver(A, ans, idx + 1, ssf)
 
 # Official
